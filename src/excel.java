8bf981984d903cbfe1d1bd2bcc36ad24852ad7eb
xzsk2 <sk308269317@outlook.com>
2018-10-18 10:25:20 +0800

    add notes

--- src/excel.java
+++ src/excel.java
@@ -7,24 +7,25 @@ import java.util.ArrayList;
 
 public class excel {
 
-    HSSFWorkbook wb = new HSSFWorkbook();
-    HSSFSheet sheet = wb.createSheet("微博");
-    HSSFRow row = sheet.createRow(0);
-    HSSFCellStyle style = wb.createCellStyle();
-    HSSFCell cell;
+    private HSSFWorkbook wb = new HSSFWorkbook();
+    private HSSFSheet sheet = wb.createSheet("微博");
+    private HSSFRow row = sheet.createRow(0);
+    private HSSFCellStyle style = wb.createCellStyle();
+    private HSSFCell cell;
 
-    public excel(){
+    public excel() {
         style.setAlignment(HorizontalAlignment.CENTER);  //居中
         String[] headers = {"微博ID", "昵称", "所在地", "性别", "生日", "公司", "教育", "标签", "微博认证（是/否）", "关注数", "粉丝数", "微博数", "当前等级", "经验值"};
-        for(int i = 0; i< headers.length; i++){
+        for (int i = 0; i < headers.length; i++) {
             cell = row.createCell(i);
             cell.setCellValue(headers[i]);
             cell.setCellStyle(style);
         }
     }
 
-    public void addRow(ArrayList<String> data){
-        row = sheet.createRow(sheet.getLastRowNum()+1);
+    // 添加行
+    public void addRow(ArrayList<String> data) {
+        row = sheet.createRow(sheet.getLastRowNum() + 1);
         for (int i = 0; i < data.size(); i++) {
             cell = row.createCell(i);
             cell.setCellValue(data.get(i));
@@ -32,13 +33,13 @@ public class excel {
         }
     }
 
-    public void output(){
+    // 输出处理过的excel
+    public void output() {
         try {
             FileOutputStream fout = new FileOutputStream("src/excel.xls");
             wb.write(fout);
             fout.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("Excel文件生成成功...");
